899d47e1ab2ad3154b2dcc1ba5dd64ef83d7a94b
Kerim Tuncer <47334351+kerimtdev@users.noreply.github.com>
2024-04-03 10:11:42 +0300

    feat: extract indicator-execution logic to `util`

---
+++ src/util.ts
@@ -0,0 +1,34 @@
+import BigNumber from "bignumber.js";
+import { KlinePayload } from "./type";
+import { initialOHLCV } from "./constant";
+
+// @ts-ignore
+import PrecisionTradingIndicators from "precision-trading-indicators";
+
+export const buildIndicatorOutput = (data: KlinePayload[], period = 14) => {
+  const indicator = new PrecisionTradingIndicators(BigNumber);
+
+  const ohlcv = data.reduce(
+    (
+      acc: typeof initialOHLCV,
+      [timestamp, open, high, low, close, volume]
+    ) => ({
+      timestamp: [...acc.timestamp, timestamp],
+      open: [...acc.open, BigNumber(open)],
+      high: [...acc.high, BigNumber(high)],
+      low: [...acc.low, BigNumber(low)],
+      close: [...acc.close, BigNumber(close)],
+      volume: [...acc.volume, volume],
+    }),
+    initialOHLCV
+  );
+
+  const bollinger = indicator.BOLLINGER_BANDS(ohlcv.close, 20, 2);
+  const candlestick = indicator.getCandlestickPattern(ohlcv);
+  const trend = indicator.getTrend(bollinger.mid, period);
+
+  return {
+    trend,
+    candlestick,
+  };
+};
